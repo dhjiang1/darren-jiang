84f386de6ffada97e10263b791904f2c638fa9c7
Darren Jiang <darren.jiang135@gmail.com>
2023-05-27 22:10:18 -0400

    css for landing page

--- personal-site/src/app/page.tsx
+++ personal-site/src/app/page.tsx
@@ -4,31 +4,33 @@ import './globals.css'
 export default function Home() {
   return (
     <main>
-      <div className="top">
-        <div className="logo">
-          <h1>DJ</h1>
-        </div>
-
-        <div className="nav-bar">
-          <h2>home</h2>
-          <h2>about</h2>
-          <h2>skills</h2>
-          <h2>experience</h2>
-          <h2>contact</h2>
-        </div>
+      <div className="logo">
+        <h1>DJ</h1>
       </div>
 
+      <div className="nav-bar">
+        <div className="section">home</div>
+        <div className="section">about</div>
+        <div className="section">skills</div>
+        <div className="section">experience</div>
+        <div className="section">contact</div>
+      </div>
+
+
       <div className="title-intro">
-        <h1>product manager</h1>
-        <h1>&#38;developer</h1>
-        <h2>Hi, I'm Darren Jiang! A passionate Product Manager and </h2>
-        <h2>Developer all about challenging the status quo.</h2>
+        <h1>product manager &#38; developer</h1>
+        <h2>Hi, I'm Darren Jiang! A passionate Product Manager and &#38; Developer all about challenging the status quo.</h2>
       </div>
 
       <div className="links">
-        <h1>LinkedIn</h1>
-        <h1>Github</h1>
-  
+        <div className="socials">
+          <h1>LinkedIn</h1>
+          <h1>Github</h1>
+        </div>
+
+        <div className="email">
+          <h1>darren.jiang135@gmail.com</h1>
+      </div>
       </div>
 
     </main>
